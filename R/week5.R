fca2583b96f47eb7e3c46227c542f44f9d62f17f
Tjknickel <knick071@umn.edu>
2026-02-21 14:02:30 -0600

    Finished data import

--- R/week5.R
+++ R/week5.R
@@ -1,1 +1,10 @@
 # Script Settings and Resources 
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+library(tidyverse)
+
+# Data Import 
+Adata_tbl <- read_delim(file = "../data/Aparticipants.dat", delim = "-", col_names = c("casenum", "parnum", "stimver", "datadate", "qs"))
+Anotes_tbl <- read_csv(file = "../docs/Anotes.csv")
+Bdata_tbl <- read_delim(file = "../data/Bparticipants.dat", delim = "\t", col_names = c("casenum", "parnum", "stimver", "datadate", paste0("q", 1:10))) #paste0("q", 1:10), "q1", "q2", "q3", "q4", "q5", "q6", "q7", "q8", "q9", "q10"
+Bnotes_tbl <- read_delim(file = "../docs/Bnotes.txt", delim = "\t")
+ 
