e79da4f3883b82004e299df38720469468b26fa6
Tjknickel <knick071@umn.edu>
2026-02-21 14:28:13 -0600

    Finished cleaning data table A

--- R/week5.R
+++ R/week5.R
@@ -5,6 +5,13 @@ library(tidyverse)
 # Data Import 
 Adata_tbl <- read_delim(file = "../data/Aparticipants.dat", delim = "-", col_names = c("casenum", "parnum", "stimver", "datadate", "qs"))
 Anotes_tbl <- read_csv(file = "../docs/Anotes.csv")
-Bdata_tbl <- read_delim(file = "../data/Bparticipants.dat", delim = "\t", col_names = c("casenum", "parnum", "stimver", "datadate", paste0("q", 1:10))) #paste0("q", 1:10), "q1", "q2", "q3", "q4", "q5", "q6", "q7", "q8", "q9", "q10"
+Bdata_tbl <- read_delim(file = "../data/Bparticipants.dat", delim = "\t", col_names = c("casenum", "parnum", "stimver", "datadate", paste0("q", 1:10))) 
 Bnotes_tbl <- read_delim(file = "../docs/Bnotes.txt", delim = "\t")
- 
+
+# Data Cleaning 
+Aclean_tbl <- Adata_tbl %>% 
+   separate(col = qs, into = paste0("q", 1:5), sep = "-") %>%
+   mutate(datadate = mdy_hms(datadate)) %>%
+   mutate(across(q1:q5, as.integer)) %>%
+   inner_join(Anotes_tbl, by = "parnum") %>%
+   filter(is.na(notes))
