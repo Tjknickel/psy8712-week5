e22366141be2c3bd70bd808a67deed30ae09e4f3
Tjknickel <knick071@umn.edu>
2026-02-22 19:27:56 -0600

    Finished project

--- R/week5.R
+++ R/week5.R
@@ -4,9 +4,9 @@ library(tidyverse)
 
 # Data Import 
 Adata_tbl <- read_delim(file = "../data/Aparticipants.dat", delim = "-", col_names = c("casenum", "parnum", "stimver", "datadate", "qs"))
-Anotes_tbl <- read_csv(file = "../docs/Anotes.csv")
+Anotes_tbl <- read_csv(file = "../data/Anotes.csv")
 Bdata_tbl <- read_delim(file = "../data/Bparticipants.dat", delim = "\t", col_names = c("casenum", "parnum", "stimver", "datadate", paste0("q", 1:10))) 
-Bnotes_tbl <- read_delim(file = "../docs/Bnotes.txt", delim = "\t")
+Bnotes_tbl <- read_delim(file = "../data/Bnotes.txt", delim = "\t")
 
 # Data Cleaning 
 Aclean_tbl <- Adata_tbl %>% 
@@ -15,3 +15,10 @@ Aclean_tbl <- Adata_tbl %>%
    mutate(across(q1:q5, as.integer)) %>%
    inner_join(Anotes_tbl, by = "parnum") %>%
    filter(is.na(notes))
+ABclean_tbl <- Bdata_tbl %>% 
+   mutate(datadate = mdy_hms(datadate)) %>%
+   mutate(across(q1:q10, as.integer)) %>%
+   inner_join(Bnotes_tbl, by = "parnum") %>%
+   filter(is.na(notes)) %>%
+   bind_rows(Aclean_tbl, .id = "lab") %>% 
+   select(-notes)
